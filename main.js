1d5347634472b0fc84abb1b92eee45abfe4abde8
CodeNKasch <kasch.dev@gmail.com>
2019-09-09 08:38:58 +0200

    font changes

--- main.js
+++ main.js
@@ -4,12 +4,19 @@ Vue.component('project-component',{
   methods:{},
   template:`
     <div class="project" >
-        <h3><i>{{project.name}}</i></h3>
-        <i>{{project.status}}</i> -
-        <i>{{project.date}}</i>
-        <div class="card">
-        <img :src="project.img">
+    <h3><i><a :href="project.github">{{project.name}}</a></i></h3>
+      
+      <div class="card">
+        <div class="front">
+            <img :src="project.img"> 
+        </div>
+        <div class="back">
+        
+        </div>
       </div>
+      </br>
+        <i>{{project.status}}</i> -
+        <i>{{project.date}}</i>        
     </div>
   `
 });
@@ -19,9 +26,9 @@ var app = new Vue({
   data:{
     message:'Work in progress',
     projects:[
-      {name:'Project 1',date:'22.12.2018',status:'PLANNING',img:'rock600x400.jpg'},
-      {name:'Project 2',date:'22.12.2018',status:'PLANNING',img:'vue.png'},
-      {name:'Project 3',date:'01.01.1970',status:'WIP',img:'stravisitx.PNG'}
+      {name:'TPHeaven',date:'22.12.2018',status:'PLANNING',img:'rock600x400.jpg', github:'https://github.com/CodeNKasch/TPHeaven', description: 'Login to Heaven HR via Android.'},
+      {name:'Landing page',date:'22.12.2018',status:'PLANNING',img:'vue.png',github: 'https://github.com/CodeNKasch/CodeNKasch.github.io', description: 'Github landing page.'},
+      {name:'Stravistix for strava',date:'01.01.1970',status:'WIP',img:'stravisitx.PNG', github: 'https://github.com/CodeNKasch/stravistix', description: 'Only a adaption of chrome-webext code to firefox'}
     ]
   },
   computed:{
